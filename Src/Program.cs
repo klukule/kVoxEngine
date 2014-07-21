0bac4a4bbfcee3d88301ff37972554416d533882
Lukáš Jech <klukule@moowdesign.eu>
2014-07-21 21:30:35 +0200

    Initial Commit

---
+++ Src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenGL;
+using Tao.FreeGlut;
+
+namespace kVoxEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(1280, 768);
+            Glut.glutCreateWindow("kVoxGame");
+
+            Glut.glutMainLoop();
+        }
+    }
+}
